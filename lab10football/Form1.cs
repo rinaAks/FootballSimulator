79983541bc35fd603c88191caafda6c6e47852b6
rinaAks <128968330+rinaAks@users.noreply.github.com>
2024-04-25 20:52:29 +0700

    Изменилась структура хранения команд

--- lab10football/Form1.cs
+++ lab10football/Form1.cs
@@ -10,11 +10,16 @@ namespace lab10football
     public partial class Form1 : Form
     {
         Random random = new Random();
-        List<string> names = new List<string> { "звёздочки", "фиалки", "подсолнухи", "ромашки", "бабочки", "ягодки", "карамельки", "барселона" };
+        List<Team> teams = new List<Team>();
+        List<int> scores = new List<int>();
+        List<int> winRound1lyambdaI = new List<int> { };
+        List<string> winRound1name = new List<string>();
+        int matchCounter = 0;
 
         public Form1()
         {
             InitializeComponent();
+            InitializeData();
             foreach (Control control in panel1.Controls)
             {
                 if (control is Label)
@@ -23,30 +28,29 @@ namespace lab10football
                     label.Text = "Команда";
                 }
             }
-            names = names.OrderBy(x => random.Next()).ToList();
+            teams = teams.OrderBy(x => random.Next()).ToList();
 
             int ind = 0;
             Label[] labels = { label1, label2, label3, label4, label5, label6, label7, label8 };
             foreach (Label label in labels)
             {
-                label.Text = names[ind];
+                label.Text = teams[ind].Name;
                 ind += 1;
             }
-            /*
-            foreach (Label label in panel2.Controls.OfType<Label>().Take(4))
-            {
-                //int randomNumber = random.Next(0, names.Count);  // генерируем случайное число в пределах коллекции значений
-                label.Text = names[ind];  // присваиваем значение из списка значения лейблу
-                ind += 1;
-            }*/
-
         }
 
-        List<double> lyambdas = new List<double> { 2, 4, 1, 3, 2, 3, 2, 4 };
-        List<int> scores = new List<int>();
-        List<int> winRound1lyambdaI = new List<int> { };
-        List<string> winRound1name = new List<string>();
-        int matchCounter = 0;
+        private void InitializeData()
+        {
+            teams.Add(new Team("звёздочки", 2));
+            teams.Add(new Team("фиалки", 4));
+            teams.Add(new Team("ромашки", 1));
+            teams.Add(new Team("бабочки", 3));
+            teams.Add(new Team("ягодки", 2));
+            teams.Add(new Team("подсолнухи", 3));
+            teams.Add(new Team("карамельки", 2));
+            teams.Add(new Team("барселона", 4));
+        }
+
         private void btNext_Click(object sender, EventArgs e)
         {
             if (matchCounter == 0) playMatch(label1, label2, label12, 0);
@@ -57,6 +61,7 @@ namespace lab10football
             else if (matchCounter == 5) playMatch(label9, label10, label14, winRound1lyambdaI[2]);
             matchCounter++;
         }   
+
         // мысль: сделать класс с полями label и лямбда, работать с объектами класса
         public void playMatch(Label lbT1, Label lbT2, Label lbWinner, int lyambdaI)
         {
@@ -73,7 +78,7 @@ namespace lab10football
                     // int value = random.Next(0, 5);
                     double value = (double)random.NextDouble();
                     sum += Math.Log10(value);
-                    if (sum < (-1 * lyambdas[lyambdaI + i]))
+                    if (sum < (-1 * teams[lyambdaI + i].Lyambda))
                     {
                         break;
                     }
@@ -84,8 +89,8 @@ namespace lab10football
             lbScore2.Text = scores[1].ToString();
             if (matchCounter < 4)
             {
-                lbTeam1.Text = names[lyambdaI];
-                lbTeam2.Text = names[lyambdaI + 1];
+                lbTeam1.Text = teams[lyambdaI].Name;
+                lbTeam2.Text = teams[lyambdaI + 1].Name;
             }
             else if(matchCounter == 4) 
             {
@@ -101,20 +106,20 @@ namespace lab10football
             lbWinner.ForeColor = Color.Green;
             if (scores[0] > scores[1])
             {
-                lbWinner.Text = names[lyambdaI];
+                lbWinner.Text = teams[lyambdaI].Name;
                 if(matchCounter < 4)
                 {
                     winRound1lyambdaI.Add(lyambdaI);
-                    winRound1name.Add(names[lyambdaI]);
+                    winRound1name.Add(teams[lyambdaI].Name);
                 }
             }
             else
             {
-                lbWinner.Text = names[lyambdaI + 1];
+                lbWinner.Text = teams[lyambdaI + 1].Name;
                 if (matchCounter < 4)
                 {
                     winRound1lyambdaI.Add(lyambdaI + 1);
-                    winRound1name.Add(names[lyambdaI + 1]);
+                    winRound1name.Add(teams[lyambdaI + 1].Name);
                 }
             }
         }
---
+++ lab10football/Team.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10football
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// Среднее количество голов команды
+        /// (нужно для Пуассоновского распределения)
+        /// </summary>
+        public double Lyambda { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="lyambda"></param>
+        public Team(string name, double lyambda)
+        {
+            Name = name;
+            Lyambda = lyambda;
+        }
+    }
+}
