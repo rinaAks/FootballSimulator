fd988c85c2a8c0c85449ee5e997575d2f47c0cb4
rinaAks <128968330+rinaAks@users.noreply.github.com>
2024-04-25 20:10:10 +0700

    Add project files.

---
+++ lab10football/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace lab10football
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lbScore1 = new System.Windows.Forms.Label();
+            this.lbScore2 = new System.Windows.Forms.Label();
+            this.btNext = new System.Windows.Forms.Button();
+            this.lbTeam1 = new System.Windows.Forms.Label();
+            this.lbTeam2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(23, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(23, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(23, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(23, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(3, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(3, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 32);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(3, 207);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 32);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(3, 301);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 32);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(727, 89);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(78, 32);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "label9";
+            this.label9.Visible = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(727, 269);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(91, 32);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "label10";
+            this.label10.Visible = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(207, 269);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(91, 32);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "label11";
+            this.label11.Visible = false;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(207, 89);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(91, 32);
+            this.label12.TabIndex = 10;
+            this.label12.Text = "label12";
+            this.label12.Visible = false;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label13.Location = new System.Drawing.Point(381, 171);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(91, 32);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "label13";
+            this.label13.Visible = false;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label14.Location = new System.Drawing.Point(567, 171);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(91, 32);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "label14";
+            this.label14.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(178, 425);
+            this.panel1.TabIndex = 14;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(912, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(173, 427);
+            this.panel2.TabIndex = 15;
+            // 
+            // lbScore1
+            // 
+            this.lbScore1.AutoSize = true;
+            this.lbScore1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbScore1.Location = new System.Drawing.Point(413, 430);
+            this.lbScore1.Name = "lbScore1";
+            this.lbScore1.Size = new System.Drawing.Size(27, 32);
+            this.lbScore1.TabIndex = 16;
+            this.lbScore1.Text = "0";
+            this.lbScore1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lbScore2
+            // 
+            this.lbScore2.AutoSize = true;
+            this.lbScore2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbScore2.Location = new System.Drawing.Point(598, 430);
+            this.lbScore2.Name = "lbScore2";
+            this.lbScore2.Size = new System.Drawing.Size(27, 32);
+            this.lbScore2.TabIndex = 17;
+            this.lbScore2.Text = "0";
+            this.lbScore2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btNext
+            // 
+            this.btNext.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btNext.Location = new System.Drawing.Point(58, 475);
+            this.btNext.Name = "btNext";
+            this.btNext.Size = new System.Drawing.Size(120, 52);
+            this.btNext.TabIndex = 4;
+            this.btNext.Text = "Следующий\r\nматч";
+            this.btNext.UseVisualStyleBackColor = true;
+            this.btNext.Click += new System.EventHandler(this.btNext_Click);
+            // 
+            // lbTeam1
+            // 
+            this.lbTeam1.AutoSize = true;
+            this.lbTeam1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbTeam1.Location = new System.Drawing.Point(381, 482);
+            this.lbTeam1.Name = "lbTeam1";
+            this.lbTeam1.Size = new System.Drawing.Size(93, 32);
+            this.lbTeam1.TabIndex = 18;
+            this.lbTeam1.Text = "Score 1";
+            // 
+            // lbTeam2
+            // 
+            this.lbTeam2.AutoSize = true;
+            this.lbTeam2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbTeam2.Location = new System.Drawing.Point(567, 482);
+            this.lbTeam2.Name = "lbTeam2";
+            this.lbTeam2.Size = new System.Drawing.Size(93, 32);
+            this.lbTeam2.TabIndex = 19;
+            this.lbTeam2.Text = "Score 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1146, 563);
+            this.Controls.Add(this.lbTeam2);
+            this.Controls.Add(this.lbTeam1);
+            this.Controls.Add(this.btNext);
+            this.Controls.Add(this.lbScore2);
+            this.Controls.Add(this.lbScore1);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbScore1;
+        private System.Windows.Forms.Label lbScore2;
+        private System.Windows.Forms.Button btNext;
+        private System.Windows.Forms.Label lbTeam1;
+        private System.Windows.Forms.Label lbTeam2;
+    }
+}
+
---
+++ lab10football/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace lab10football
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        List<string> names = new List<string> { "звёздочки", "фиалки", "подсолнухи", "ромашки", "бабочки", "ягодки", "карамельки", "барселона" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (Control control in panel1.Controls)
+            {
+                if (control is Label)
+                {
+                    Label label = (Label)control;
+                    label.Text = "Команда";
+                }
+            }
+            names = names.OrderBy(x => random.Next()).ToList();
+
+            int ind = 0;
+            Label[] labels = { label1, label2, label3, label4, label5, label6, label7, label8 };
+            foreach (Label label in labels)
+            {
+                label.Text = names[ind];
+                ind += 1;
+            }
+            /*
+            foreach (Label label in panel2.Controls.OfType<Label>().Take(4))
+            {
+                //int randomNumber = random.Next(0, names.Count);  // генерируем случайное число в пределах коллекции значений
+                label.Text = names[ind];  // присваиваем значение из списка значения лейблу
+                ind += 1;
+            }*/
+
+        }
+
+        List<double> lyambdas = new List<double> { 2, 4, 1, 3, 2, 3, 2, 4 };
+        List<int> scores = new List<int>();
+        List<int> winRound1lyambdaI = new List<int> { };
+        List<string> winRound1name = new List<string>();
+        int matchCounter = 0;
+        private void btNext_Click(object sender, EventArgs e)
+        {
+            if (matchCounter == 0) playMatch(label1, label2, label12, 0);
+            else if (matchCounter == 1) playMatch(label3, label4, label11, 2);
+            else if (matchCounter == 2) playMatch(label5, label6, label9, 4);
+            else if (matchCounter == 3) playMatch(label7, label8, label10, 6);
+            else if (matchCounter == 4) playMatch(label12, label11, label13, winRound1lyambdaI[0]);
+            else if (matchCounter == 5) playMatch(label9, label10, label14, winRound1lyambdaI[2]);
+            matchCounter++;
+        }   
+        // мысль: сделать класс с полями label и лямбда, работать с объектами класса
+        public void playMatch(Label lbT1, Label lbT2, Label lbWinner, int lyambdaI)
+        {
+            scores.Clear();
+            scores = new List<int>(new int[2]);
+            scores[0] = 0; scores[1] = 0;
+            lbT1.ForeColor = Color.Blue;
+            lbT2.ForeColor = Color.Blue;
+            for (int i = 0; i < 2; i++)
+            {
+                double sum = 0;
+                while (true)
+                {
+                    // int value = random.Next(0, 5);
+                    double value = (double)random.NextDouble();
+                    sum += Math.Log10(value);
+                    if (sum < (-1 * lyambdas[lyambdaI + i]))
+                    {
+                        break;
+                    }
+                    scores[i] += 1;
+                }
+            }
+            lbScore1.Text = scores[0].ToString();
+            lbScore2.Text = scores[1].ToString();
+            if (matchCounter < 4)
+            {
+                lbTeam1.Text = names[lyambdaI];
+                lbTeam2.Text = names[lyambdaI + 1];
+            }
+            else if(matchCounter == 4) 
+            {
+                lbTeam1.Text = winRound1name[0];
+                lbTeam2.Text = winRound1name[1];
+            }
+            else if (matchCounter == 5)
+            {
+                lbTeam1.Text = winRound1name[2];
+                lbTeam2.Text = winRound1name[3];
+            }
+            lbWinner.Visible = true;
+            lbWinner.ForeColor = Color.Green;
+            if (scores[0] > scores[1])
+            {
+                lbWinner.Text = names[lyambdaI];
+                if(matchCounter < 4)
+                {
+                    winRound1lyambdaI.Add(lyambdaI);
+                    winRound1name.Add(names[lyambdaI]);
+                }
+            }
+            else
+            {
+                lbWinner.Text = names[lyambdaI + 1];
+                if (matchCounter < 4)
+                {
+                    winRound1lyambdaI.Add(lyambdaI + 1);
+                    winRound1name.Add(names[lyambdaI + 1]);
+                }
+            }
+        }
+    }
+}
